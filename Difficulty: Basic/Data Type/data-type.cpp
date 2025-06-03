e3e976868cbdf4549a0622a8d4f70aa8c7ef41a2
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-06-03 13:17:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Data Type/data-type.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int dataTypeSize(string str) {
+        // your code here
+        if (str == "Character"){ return sizeof(char) ; }
+        else if (str == "Integer") { return sizeof(int) ; }
+        else if (str == "Long") {  return sizeof(long) ;  }
+        else if (str == "Float") { return sizeof(float) ;  }
+        else if ( str == "Double") { return sizeof(double) ;  }
+        
+    }
+}; 
+
