e972dafc6daaf0d53339ffaa60e099286d00d358
Rohansejwal64 <137511240+Rohansejwal64@users.noreply.github.com>
2025-09-09 12:34:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,27 +1,16 @@
 class Solution {
   public:
-    bool solve(vector<int>& arr, int k,vector<vector<int>> &dp,int i){
-        if(k==0) return true;
-        if(i==0){
-            if(arr[0]==k) return true;
-            else{
-                return false;
-            }
-        }
-        if(dp[i][k] !=-1) return dp[i][k];
-        
-        bool nt=solve(arr,k,dp,i-1);
-        if(nt==true) return nt;
-        bool t=false;
-        if(k>=arr[i]){
-            t=solve(arr,k-arr[i],dp,i-1);
-        }
-        return dp[i][k]=(nt || t);
-    }
     bool isSubsetSum(vector<int>& arr, int k) {
-        // code here
-        int n=arr.size();
-        vector<vector<int>> dp(n,vector<int>(k+1,-1));
-        return solve(arr,k,dp,n-1);
-    }
+    int n=arr.size();
+    vector<vector<int>> dp(n,vector<int>(k+1,-1));
+    for(int i=0;i<=n-1;i++){ dp[i][0]=1; } 
+    for(int i=1;i<=k;i++){
+        if(arr[0]==i){ dp[0][i]=1;
+        }
+        else{
+            dp[0][i]=0;
+        }
+        }
+    for(int i=1;i<=n-1;i++){ for(int j=1;j<=k;j++){ bool nt=dp[i-1][j]; if(nt==true) dp[i][j]=1; bool t=false; if(j>=arr[i]){ t= dp[i-1][j-arr[i]]; } dp[i][j]=(nt || t); } } return dp[n-1][k];
+}
 };
